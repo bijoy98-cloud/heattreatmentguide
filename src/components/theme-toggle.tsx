b588321c3da992d5ca96a365a5982bd8692026c6
Bijoy Saha <bijoy98@gmail.com>
2025-11-28 18:07:58 +0000

    "use client"
    import * as React from "react"
    import { useTheme } from "n

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,4 +1,41 @@
-// This is a placeholder file. We will fill this in next.
+"use client"
+
+import * as React from "react"
+import { useTheme } from "next-themes"
+import { Moon, Sun } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuItem,
+  DropdownMenuSubTrigger,
+  DropdownMenuPortal,
+} from "@/components/ui/dropdown-menu"
+
 export function ThemeToggle() {
-  return null;
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenuSub>
+        <DropdownMenuSubTrigger>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="ml-2">Theme</span>
+        </DropdownMenuSubTrigger>
+        <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+                </DropdownMenuItem>
+            </DropdownMenuSubContent>
+        </DropdownMenuPortal>
+    </DropdownMenuSub>
+  )
 }
