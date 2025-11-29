878f9fef862b8bf0e0be19c1820c8531bc2d439a
Bijoy Saha <bijoy98@gmail.com>
2025-11-29 07:39:25 +0000

    change the color all tab to #0000ff text remain white (_for element <Pri

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -67,13 +67,13 @@ export default function AboutPage() {
         </div>
 
         <Tabs defaultValue="about-us" className="w-full">
-          <TabsList className="h-auto justify-start flex-wrap">
-            <TabsTrigger value="about-us" className="data-[state=active]:bg-blue-500 data-[state=active]:text-white">About Us</TabsTrigger>
-            <TabsTrigger value="user-manual" className="data-[state=active]:bg-blue-500 data-[state=active]:text-white">User Manual</TabsTrigger>
-            <TabsTrigger value="terms" className="data-[state=active]:bg-blue-500 data-[state=active]:text-white">Terms of Service</TabsTrigger>
-            <TabsTrigger value="privacy" className="data-[state=active]:bg-blue-500 data-[state=active]:text-white">Privacy Policy</TabsTrigger>
-            <TabsTrigger value="cancellation" className="data-[state=active]:bg-blue-500 data-[state=active]:text-white">Cancellation</TabsTrigger>
-            <TabsTrigger value="contact" className="data-[state=active]:bg-blue-500 data-[state=active]:text-white">Contact Us</TabsTrigger>
+          <TabsList className="h-auto justify-start flex-wrap bg-blue-500 text-white">
+            <TabsTrigger value="about-us" className="data-[state=active]:bg-blue-600 data-[state=active]:text-white">About Us</TabsTrigger>
+            <TabsTrigger value="user-manual" className="data-[state=active]:bg-blue-600 data-[state=active]:text-white">User Manual</TabsTrigger>
+            <TabsTrigger value="terms" className="data-[state=active]:bg-blue-600 data-[state=active]:text-white">Terms of Service</TabsTrigger>
+            <TabsTrigger value="privacy" className="data-[state=active]:bg-blue-600 data-[state=active]:text-white">Privacy Policy</TabsTrigger>
+            <TabsTrigger value="cancellation" className="data-[state=active]:bg-blue-600 data-[state=active]:text-white">Cancellation</TabsTrigger>
+            <TabsTrigger value="contact" className="data-[state=active]:bg-blue-600 data-[state=active]:text-white">Contact Us</TabsTrigger>
           </TabsList>
 
           <TabsContent value="about-us">
